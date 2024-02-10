02d8acf985f727109cf3459e37ce0d6b5c965574
bullydd <clubdeveloppeursenegal@gmail.com>
2024-02-10 10:25:34 +0100

    help infos added

--- src/main.adb
+++ src/main.adb
@@ -1,4 +1,8 @@
+with Ada.Text_IO;           use Ada.Text_IO;
+
 procedure Main is
 begin
-    null;
+    Put_Line ("Gtkada-tutorial serie :" & ASCII.CR &
+    "This serie of tutorial shows the use of Gtkada which is Ada binding," & ASCII.CR &
+    " of Gtk. We start from scratch on GUI creation. At the end we will build a simple game");
 end Main;
