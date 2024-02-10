fb5237bacc01b1084bcb5d008a410976270fccde
bullydd <clubdeveloppeursenegal@gmail.com>
2024-02-10 11:00:08 +0100

    Add tutoriel_02.adb

---
+++ src/tutoriel_02.adb
@@ -0,0 +1,24 @@
+--
+--  Tutoriel-02 
+--  In GUI programming we are going to write a lot of code.
+--  and doing this in a single file is not a good pratice.
+--  It can become quickly a big mess. In this second tutorial
+--  Ada is a multi-paradigm programming language with a lot of
+--  interesting features such strong typing, POO, tasking, memory
+--  management...
+--  Here I will create a simple window with an embedded button 
+--  by using POO approach. Package is an Ada facility that helps
+--  developers to manage their code in an efficient way.
+--  A package is compiled unit which is divided into two parts 
+--  (spec and body).
+--  Package specification (spec) can be divide into two parts 
+--  (public and private). Within it you can find type declaration
+--  sub-program declaration.
+-- 
+
+with Gtk.Main;
+
+procedure Tutoriel_02 is
+begin
+    null;
+end Tutoriel_02;
