0d44c993721d822dd3c009ab70892cb17982f25b
bullydd <clubdeveloppeursenegal@gmail.com>
2024-04-10 19:26:50 +0100

    Ajout de commentaires en francais

--- src/tutoriel_02.adb
+++ src/tutoriel_02.adb
@@ -1,20 +1,14 @@
---
---  Tutoriel-02 
---  In GUI programming we are going to write a lot of code.
---  and doing this in a single file is not a good pratice.
---  It can become quickly a big mess. In this second tutorial
---  Ada is a multi-paradigm programming language with a lot of
---  interesting features such strong typing, POO, tasking, memory
---  management...
---  Here I will create a simple window with an embedded button 
---  by using POO approach. Package is an Ada facility that helps
---  developers to manage their code in an efficient way.
---  A package is compiled unit which is divided into two parts 
---  (spec and body).
---  Package specification (spec) can be divide into two parts 
---  (public and private). Within it you can find type declaration
---  sub-program declaration.
--- 
+--------------------------------------------------------------------------------------------------------------
+-- Tutoriel-02 
+-- En programmation GUI, nous allons écrire beaucoup de code. Faire cela en un seul fichier 
+-- n'est pas une bonne pratique. Cela peut rapidement devenir un gros gâchis. 
+-- Dans ce deuxième tutoriel nous allons créer une fenêtre simple avec un bouton intégré en 
+-- utilisant l'approche POO. 
+-- Un paquetage est une fonctionnalité Ada qui permet aux développeurs à gérer leur code de manière efficace.
+-- Un package est une unité compilée divisée en deux parties (spécification et corps).
+-- La spécification du package (spec) peut être divisée en deux parties (publique et privée). À l'intérieur, 
+-- vous pouvez trouver la déclaration de sous-programme de déclaration de type.
+---------------------------------------------------------------------------------------------------------------
 
 with Gtk.Main;
 
